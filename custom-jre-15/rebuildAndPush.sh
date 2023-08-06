7fd9df127e52d07b5309bd89965d03fbfc0beacd
Sachin Goyal <sachin@Sachins-MacBook-Pro-M2.local>
2023-08-06 22:48:11 +0100

    add platform

--- custom-jre-15/rebuildAndPush.sh
+++ custom-jre-15/rebuildAndPush.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-# Example local: ./rebuildAndPush localhost:5000 apps-template latest
+# Example local: ./rebuildAndPush localhost:5500 apps-template latest
 # Example production: ./rebuildAndPush 1234567890123.ecr.us-west-2.amazonaws.com apps-template 1.2
 
 REGISTRY=$1
-SERVICE=${PWD##*/}
 NAMESPACE=$2
 VERSION=$3
+SERVICE=${PWD##*/}
+PLATFORM=linux/x86_64
 
 # delete existing containers/images on local docker registry.
 if echo "$1" | grep -q "localhost:"; then
@@ -14,7 +15,7 @@ if echo "$1" | grep -q "localhost:"; then
   echo ">>> Existing Images/Containers Deleted"
 fi
 
-docker build --no-cache -t $REGISTRY'/'$NAMESPACE'-'$SERVICE':'$VERSION .
+docker build --no-cache --platform $PLATFORM -t $REGISTRY'/'$NAMESPACE'-'$SERVICE':'$VERSION .
 echo ">>> New Docker Image Built"
 
 docker push $REGISTRY'/'$NAMESPACE'-'$SERVICE':'$VERSION
