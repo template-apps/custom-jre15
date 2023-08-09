ee6074d5c0fa56ef8f87483ad9556b16c3b1d5b9
Sachin Goyal <sachin.goyal.se@gmail.com>
2023-08-09 11:56:07 +0100

    solidified scripts

--- custom-jre-20/rebuildAndPush.sh
+++ custom-jre-20/rebuildAndPush.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Example: ./rebuildAndPush.sh -r localhost:5500 -n apps-template -v latest -p linux/x86_64
-# Example: ./rebuildAndPush.sh -r 881387567440.dkr.ecr.us-east-1.amazonaws.com -n apps-template -v 0.1 -p linux/x86_64
+# Example: ./rebuildAndPush.sh -r 881387567440.dkr.ecr.us-east-1.amazonaws.com -n apps-template -v latest -p linux/x86_64
 
 ARCH=""
 
